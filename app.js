178b23636f43146c1c1e1ff093d08281161c72c0
saksham kshatri <sakshamkshatri@Shauryas-MacBook-Air.local>
2023-08-08 19:24:44 +0545

    created user model, register and login endpoint done with JWT

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const dbConnect = require("./db/dbConnect");
 const User = require("./db/userModel");
+const auth = require('./auth');
 
 dbConnect();
+
+// body parser configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Home page");
@@ -35,6 +43,7 @@ app.post("/register", (request, response) => {
         })
         // catch error if the new user wasn't added successfully to the database
         .catch((error) => {
+          console.log(error);
           response.status(500).send({
             message: "Error creating user",
             error,
@@ -43,7 +52,6 @@ app.post("/register", (request, response) => {
     })
     // catch error if the password hash isn't successful
     .catch((e) => {
-      console.log("error in hashing");
       response.status(500).send({
         message: "Password was not hashed successfully",
         e,
@@ -51,6 +59,68 @@ app.post("/register", (request, response) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log("Server listening on port 5000");
+app.post("/login", (request, response) => {
+  // check if email exists
+  User.findOne({ email: request.body.email })
+
+    // if email exists
+    .then((user) => {
+      // compare the password entered and the hashed password found
+      bcrypt
+        .compare(request.body.password, user.password)
+
+        // if the passwords match
+        .then((passwordCheck) => {
+          // check if password matches
+          if (!passwordCheck) {
+            return response.status(400).send({
+              message: "Passwords does not match",
+              error,
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          //   return success response
+          response.status(200).send({
+            message: "Login Successful",
+            email: user.email,
+            token,
+          });
+        })
+        // catch error if password does not match
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords does not match",
+            error,
+          });
+        });
+    })
+    // catch error if email does not exist
+    .catch((e) => {
+      response.status(404).send({
+        message: "Email not found",
+        e,
+      });
+    });
+});
+
+app.get("/free-endpoint", (req, res) => {
+  response.json({ message: "you are free to access this endpoint" });
+});
+
+app.get("/auth-endpoint", auth, (req, res) => {
+  response.json({ message: "You are authenticated to access this endpoint" });
+});
+
+app.listen(8000, () => {
+  console.log("Server listening on port 8000");
 });
